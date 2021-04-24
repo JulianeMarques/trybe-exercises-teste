e9edb2c3faff547d28b85ec03fd89701ed8dc6a9
Juliane Marques <mqs.juliane@gmail.com>
2021-04-24 17:08:04 -0300

    exercicios aulas 1

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -8,3 +8,15 @@ let base = 5;
 let altura =8;
 
 console.log(base * altura);
+
+let nota = 67;
+
+if (nota >= 80) {
+    console.log("Aprovado");
+}
+else if (nota <= 80 && nota >= 60) {
+    console.log("lista de espera");
+}
+else {
+    console.log("reprovado");
+}

059e54f01f50a8ed52dc80202b944488e60ea0a4
Juliane Marques <mqs.juliane@gmail.com>
2021-04-25 17:26:01 -0300

    exercicio aula 2

--- exercises/bloco_4/dia_2/script.js
+++ exercises/bloco_4/dia_2/script.js
@@ -1,20 +1,37 @@
-// exercicio 1
+// exercicio 1.1
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
 let menuServices = menu[1];
 console.log(menuServices);
 
-// exercicio 2
+// exercicio 1.2
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
 let indexOfPortfolio = menu.indexOf('Portfólio');
 console.log(indexOfPortfolio);
 
-// exercicio 3
+// exercicio 1.3
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
 menu.push('Contato');
 console.log(menu);
+
+// exercicio 2.1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index +=1) {
+    console.log(groceryList[index]);
+}
+
+// exercicio 2.2
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(names);
+}
+
